a4f1c753d0f2d55892f91e29b8c6024a8223f4e0
sikwan <yysskk99999@naver.com>
2024-10-14 20:32:36 +0900

    if setState x

--- lib/main.dart
+++ lib/main.dart
@@ -16,13 +16,13 @@ class _AppState extends State<App> {
   List<int> numbers = [];
 
   void onClicked() {
-    setState(
-      //약간 swift 에서의 state 같은 느낌인데. .
-      () {
-        //  counter += 1;
-        numbers.add(numbers.length);
-      },
-    );
+    //setState(
+    //약간 swift 에서의 state 같은 느낌인데. .
+    () {
+      //  counter += 1;
+      numbers.add(numbers.length);
+    };
+    // )
   }
 
   @override
